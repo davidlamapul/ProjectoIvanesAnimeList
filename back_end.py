ead46a8d34c1421d6c6e8331a28317e8885a6331
David Lama Pulido <davidlamapul@gmail.com>
2022-06-07 16:06:31 +0200

    functions backend

--- back_end.py
+++ back_end.py
@@ -1,4 +1,20 @@
-def add(id_user: int, name_anime: str, status: str, ep_w):
+def initialise_database():
+    print()
+
+
+def create_user(email: str, password: str, username: str):
+    user_id: int
+
+    return user_id
+
+
+def get_user_id(email: str, password: str):
+    user_id: int
+
+    return user_id
+
+
+def add(id_user: int, name_anime: str, status: str, ep_watched: int = 0):
     print()
 
 
