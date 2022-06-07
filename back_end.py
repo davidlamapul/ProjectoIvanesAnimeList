4bb0aafdec439cd07c8de0705bfb15f50037b3c9
David Lama Pulido <davidlamapul@gmail.com>
2022-06-07 16:34:13 +0200

    functions backend

--- back_end.py
+++ back_end.py
@@ -14,13 +14,29 @@ def get_user_id(email: str, password: str):
     return user_id
 
 
-def add(id_user: int, name_anime: str, status: str, ep_watched: int = 0):
+# todo poner todos los campos
+def create_anime(name_anime: str, ep_total: int):
     print()
 
 
-def remove():
+# todo status por defecto en viendo
+def add(user_id: int, name_anime: str, status: str = None, ep_watched: int = 0):
     print()
 
 
-def get_all():
+def remove(user_id: int, id_anime: int):
     print()
+
+
+# status y ep_watched son opcionales
+def update(user_id: int, id_anime: int, status: str = None, ep_watched: int = None):
+    if status is not None:
+        print()
+
+    if ep_watched is not None:
+        print()
+
+
+def get_all(user_id: int):
+
+    return []
