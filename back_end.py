54d7a855e808919dd499724a9c0632c9cae2b6fd
David Lama Pulido <davidlamapul@gmail.com>
2022-06-07 16:46:57 +0200

    fixing typo

--- back_end.py
+++ back_end.py
@@ -15,7 +15,7 @@ def get_user_id(email: str, password: str):
 
 
 # todo poner todos los campos
-def create_anime(name_anime: str, ep_total: int, year: int, id_secula: int = None):
+def create_anime(name_anime: str, ep_total: int, year: int, id_sequel: int = None):
     print()
 
 
